57892eeca6c1d0d4abc95eab873d5c3585b87ffe
petrushevskiyMaksim <maxinverst@gmail.com>
2024-11-24 17:33:41 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import { useState } from 'react';
-
 import './App.css';
 
 function App() {
-	const [count, setCount] = useState(0);
+	return (
+		<>
+			<h1>hello github</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio nostrum
+				quibusdam pariatur atque! Inventore, rem impedit!
+			</p>
+		</>
+	);
 }
 
 export default App;
